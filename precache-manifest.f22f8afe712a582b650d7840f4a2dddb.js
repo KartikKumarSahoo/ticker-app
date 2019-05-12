0e7116233ee61c117c797fe347202006e96deca3
Kartik Sahoo <kartikmca09@gmail.com>
2019-05-12 14:07:51 +0530

    Updates

---
+++ precache-manifest.f22f8afe712a582b650d7840f4a2dddb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1576b010397026027cedc75c12e1f298",
+    "url": "/ticker-app/demo/index.html"
+  },
+  {
+    "revision": "9f2357d5e6258b2dd8dc",
+    "url": "/ticker-app/demo/static/css/main.d611cc0d.chunk.css"
+  },
+  {
+    "revision": "d63838cb00d160f75ddc",
+    "url": "/ticker-app/demo/static/js/2.d9241121.chunk.js"
+  },
+  {
+    "revision": "9f2357d5e6258b2dd8dc",
+    "url": "/ticker-app/demo/static/js/main.46b7ec24.chunk.js"
+  },
+  {
+    "revision": "230b2e3729777efb1317",
+    "url": "/ticker-app/demo/static/js/runtime~main.9decef0a.js"
+  }
+]);
